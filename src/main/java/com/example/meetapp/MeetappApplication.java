17eab8e92aa921a1da9aa27aaac3f830ccf6ff6c
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-02-24 19:46:19 -0300

    First commit

---
+++ src/main/java/com/example/meetapp/MeetappApplication.java
@@ -0,0 +1,13 @@
+package com.example.meetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/meetapp/MeetappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.meetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
